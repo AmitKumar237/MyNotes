f0eb2ec4ecb0e1e756c7d848ecb60656b951d002
AmitKumar237 <723kumaramit@gmail.com>
2022-11-12 11:07:04 +0530

    Task Update is done

--- src/main/java/com/example/mynotes/HelloApplication.java
+++ src/main/java/com/example/mynotes/HelloApplication.java
@@ -10,7 +10,7 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("login.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Scene scene = new Scene(fxmlLoader.load(), 480, 320);
         stage.setTitle("Login");
         stage.setScene(scene);
         stage.show();
